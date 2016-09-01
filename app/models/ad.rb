740990d3646dc92da65fd4fab9e294803774b5b9
Angel Mora <eduardo.aemc@gmail.com>
2016-08-31 23:07:21 -0500

    aasm for ads

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -22,4 +22,30 @@
 
 class Ad < ApplicationRecord
   belongs_to :user
+
+  include AASM
+
+  #Maquina de estados para las ad
+  aasm column: "status" do
+  	state :published, initial: true
+  	state :toped
+  	state :premiuned
+  	state :selled
+
+  	event :top do 
+  		transitions from: :published, to: :toped
+  	end
+
+  	event :premiun do
+  		transitions from: :published, to: :premiuned
+  	end
+
+  	event :down do
+  		transitions from: [:toped, :premiuned], to: :published
+  	end
+
+  	event :sell do
+  		transitions from: [:published, :toped, :premiuned], to: :selled
+  	end
+  end
 end
