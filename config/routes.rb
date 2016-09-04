ef6f6a0563bf3d44e5a43c687b1e35373bee39bd
Angel Mora <eduardo.aemc@gmail.com>
2016-09-04 00:29:17 -0500

    name in the registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   resources :ads
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   devise_for :users, controllers:{
-    omniauth_callbacks: "users/omniauth_callbacks"
+    omniauth_callbacks: "users/omniauth_callbacks",
+    registrations:"users/registrations"
   }
   
   post "/custom_sign_up", to: "users/omniauth_callbacks#custom_sign_up"
 
   authenticated :user do
-    root 'home#index'
+    root "home#index"
   end
 
   unauthenticated :user do
